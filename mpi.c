2aa5c49b74def2aee4cee6c022cac6149cc40eb0
Yonathan Bettan <yonibettan@cdpslurm-master.csf.technion.ac.il>
2018-02-07 17:59:31 +0200

    commit 2
    Signed-off-by: Yonathan Bettan <yonibettan@cdpslurm-master.csf.technion.ac.il>

--- mpi.c
+++ mpi.c
@@ -1,29 +1,30 @@
 #include <mpi.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <stdio.h>
 
-#define N 10
-
-
-int p_strcmp(const char *str1, const char *str2) {
+int p_strcmp(char *str1, char *str2) {
 
     int numProc, rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &numProc);
 
-    int chunkSize = (N-1) / (numProc-1);
+    int chunkSize = strlen(str1) / numProc;
     int localRes, *resArr;
 
     if (rank == 0)
         resArr = malloc(numProc * sizeof(int));
 
     /* on all process stri will now point to the relevant part of original strs */
-    MPI_Scatter(str1, MPI_CHAR, chunkSize, str1, MPI_CHAR, chunkSize, /*root*/0,
+    MPI_Scatter(str1, chunkSize, MPI_CHAR, str1, chunkSize, MPI_CHAR, /*root*/0,
             MPI_COMM_WORLD);
-    MPI_Scatter(str2, MPI_CHAR, chunkSize, str2, MPI_CHAR, chunkSize, /*root*/0,
+    MPI_Scatter(str2, chunkSize, MPI_CHAR, str2, chunkSize, MPI_CHAR, /*root*/0,
             MPI_COMM_WORLD);
 
     str1[chunkSize] = '\0';
     str2[chunkSize] = '\0';
-    localRes = str_cmp(str1, str2);
+    localRes = strcmp(str1, str2);
 
     if (rank == 0) {
         MPI_Gather(&localRes, 1, MPI_INT, resArr, 1, MPI_INT, /*root*/0, MPI_COMM_WORLD);
@@ -35,4 +36,18 @@ int p_strcmp(const char *str1, const char *str2) {
         MPI_Gather(&localRes, 1, MPI_INT, NULL, 1, MPI_INT, /*root*/0, MPI_COMM_WORLD);
 
     return 0;
+}
+
+
+int main(int argc, char **argv) {
+
+    //char *str1 = "123456789abcdefg";
+    //char *str2 = "123456789abcdefg";
+
+    MPI_Init(&argc, &argv);
+
+    //assert(p_strcmp(str1, str2) == 0);
+    printf("hello\n");
+
+    MPI_Finalize();
 }
