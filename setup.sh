5bf76e93fa1404bcb6bfbdc74ac16f1b8e977365
Yoni Bettan <ybettan@redhat.com>
2020-01-14 09:53:27 -0500

    setup.sh done
    Signed-off-by: Yoni Bettan <ybettan@redhat.com>

--- setup.sh
+++ setup.sh
@@ -31,27 +31,25 @@ tests/deploy.sh
 ./kubectl patch svc thanos-receive --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"}]' -n observatorium 
 ./kubectl patch svc thanos-querier --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"}]' -n observatorium
 
-# FIXME: in root mode variable are not recognized and in minikube isn't running so we cannot delclare the variable there
 # expose thanos-receive and thanos-querier to remote hosts (ROOT ACCESS REQUIRED)
+listening_port=32502
+host_ip=`hostname -I | cut -d" " -f1`
 minikube_ip=`minikube ip`
 thanos_querier_port=`./kubectl get svc thanos-querier -n observatorium | tail -1 | cut -d":" -f2 | cut -d"/" -f1`
 thanos_receive_port=`./kubectl get svc thanos-receive -n observatorium | tail -1 | cut -d":" -f2 | cut -d"/" -f1`
-sudo -i
 
-iptables -t nat -I PREROUTING -p tcp -d 10.19.41.1 --dport $thanos_querier_port -j DNAT \
-    --to-destination $minikube_ip:$thanos_querier_port
-iptables -I FORWARD -p tcp -m tcp --sport $thanos_qurier_port -j ACCEPT
-iptables -I FORWARD -p tcp -m tcp --dport $thanos_qurier_port -j ACCEPT
-#
-## redirect all access to <svc/thanos-receive port> to <minikube ip>:<svc/thanos-receive port>
-## and add firewall rules for svc/thanos-receiver (in and out)
-iptables -t nat -I PREROUTING -p tcp -d 10.19.41.1 --dport 32502 -j DNAT \
+sudo --preserve-env=minikube_ip --preserve-env=host_ip --preserve-env=thanos_querier_port iptables -t nat -I PREROUTING \
+    -p tcp -d $host_ip --dport $thanos_querier_port -j DNAT --to-destination $minikube_ip:$thanos_querier_port
+sudo --preserve-env=thanos_querier_port iptables -I FORWARD -p tcp -m tcp --sport $thanos_querier_port -j ACCEPT
+sudo --preserve-env=thanos_querier_port iptables -I FORWARD -p tcp -m tcp --dport $thanos_querier_port -j ACCEPT
+
+sudo --preserve-env=minikube_ip --preserve-env=host_ip --preserve-env=thanos_receive_port --preserve-env=listening_port \
+    iptables -t nat -I PREROUTING -p tcp -d $host_ip --dport $listening_port -j DNAT \
     --to-destination $minikube_ip:$thanos_receive_port
-iptables -I FORWARD -p tcp -m tcp --sport $thanos_receive_port -j ACCEPT
-iptables -I FORWARD -p tcp -m tcp --dport $thanos_receive_port -j ACCEPT
-iptables -I FORWARD -p tcp -m tcp --sport 32502 -j ACCEPT
-iptables -I FORWARD -p tcp -m tcp --dport 32502 -j ACCEPT
-#
-## test connection from my mac with curl and GUI
-#curl bkr-hv01.dsal.lab.eng.bos.redhat.com:32502 (host is 10.19.41.1)
+sudo --preserve-env=thanos_receive_port iptables -I FORWARD -p tcp -m tcp --sport $thanos_receive_port -j ACCEPT
+sudo --preserve-env=thanos_receive_port iptables -I FORWARD -p tcp -m tcp --dport $thanos_receive_port -j ACCEPT
+sudo --preserve-env=listening_port iptables -I FORWARD -p tcp -m tcp --sport $listening_port -j ACCEPT
+sudo --preserve-env=listening_port iptables -I FORWARD -p tcp -m tcp --dport $listening_port -j ACCEPT
+
+# test connection from my mac with curl and GUI at "bkr-hv01.dsal.lab.eng.bos.redhat.com:32502" (host is 10.19.41.1)
 
