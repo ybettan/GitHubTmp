3537bc5bd0588c4d3390f1dfe977078c2e6df3b3
Yoni Bettan <ybettan@redhat.com>
2020-06-01 12:26:02 -0400

    auth_demo/auth_2_6_2020: Updated scripts.
    Not tested yet.
    
    Signed-off-by: Yoni Bettan <ybettan@redhat.com>

--- auth_demo/auth_2_6_2020/deploy.sh
+++ auth_demo/auth_2_6_2020/deploy.sh
@@ -1,45 +1,15 @@
 #!/bin/bash
 
-#oc create namespace minio
-#oc create namespace observatorium
-#
-##FIXME: make this a secret.yaml
-#oc -n observatorium create secret generic tls-certs \
-#    --from-file certs/server.pem \
-#    --from-file certs/server.key \
-#    --from-file certs/ca.pem
-
 oc -n openshift-monitoring create secret generic remote-write-key-cert-ca \
-    --from-file certs/client.pem \
-    --from-file certs/client.key
+    --from-file ~/certs/client.key \
+    --from-file ~/certs/client.pem \
+    --from-file ~/certs/ca.pem
 
 #oc apply -f environments/dev/manifests/
 oc expose svc/observatorium-xyz-observatorium-api-gateway -n observatorium
 
 observatoriu_svc_ip=`oc get svc/observatorium-xyz-observatorium-api-gateway -n observatorium \
     | tr -s " " | tail -1 | cut -d" " -f3`
-
-##FIXME: need to add clien cert and key to tlsConfig section - from secrets.
-##       As for today prometheus operator don't make use of this secret according
-##       to https://github.com/coreos/prometheus-operator/issues/3118
-#echo "
-#apiVersion: v1
-#kind: ConfigMap
-#metadata:
-#  name: cluster-monitoring-config
-#  namespace: openshift-monitoring
-#data:
-#  config.yaml: |
-#    prometheusK8s:
-#      remoteWrite:
-#        - url: "https://$observatoriu_svc_ip:8080/api/metrics/v1/write"
-#          writeRelabelConfigs:
-#          - sourceLabels: [__name__]
-#            replacement: seal18_OS_cluster # this is the name of the cluster
-#            targetLabel: cluster
-#          tlsConfig:
-#            insecureSkipVerify: true
-#" | oc apply -f -
 
 echo "
 apiVersion: v1
