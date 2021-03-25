53d43bae27d40b71ed12b00e239cf102c1e65f11
Yoni Bettan <ybettan@redhat.com>
2021-03-25 09:24:48 +0200

    For Johni Abarbanel
    Signed-off-by: Yoni Bettan <ybettan@redhat.com>

---
+++ mysin_FINAL.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#define ACCURACY 0.000001 // Very good!!
+
+
+double my_sin (double x)
+{
+	int sign = 1, a = 2 , b = 3;
+	double sum = 0, numerator = x, term = x, denumerator = 1;
+	while (fabs(term) > ACCURACY)
+	{
+		term = numerator/denumerator;
+		sum = sum + sign*term;
+		numerator = numerator * x * x;
+		denumerator = denumerator * a * b;
+		sign*=-1;
+		a+=2;
+		b+=2;
+	}
+	return sum;
+	
+}
+
+int main()
+{
+	double x;
+	printf("Please enter any real number between -25 to 25: \n");
+	scanf("%lf", &x);
+	printf("Your number is: %f.\nThe answer for sin(%f) with my calculation is: %f\n",x,x,
+	my_sin(x));
+	printf("The answer with the standard library is: sin(%f) = %f\n" , x, sin(x));
+	
+    //FIXME: fix the indention - the compiler gives a warning about it.
+ return 0;
+}
+
